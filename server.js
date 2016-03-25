ece286d30bdb54f0a7212feb4a9c49137fc5cc8d
Alice Jiang <becausealice@gmail.com>
2016-03-24 23:05:06 -0500

    Conquered the DOM beast and GOT THE DANG DATA!

--- server.js
+++ server.js
@@ -16,33 +16,33 @@ app.get('/', function(req, res){
     var $ = cheerio.load(html,{"normalizeWhitespace":true});
 
     // Any less specific and Cheerio returns data from extra elements
-    $('#wiki-body .markdown-body > ul > li').each(function(){
-      var data = {};
+    $('#wiki-body .markdown-body > ul > li').find('a').each(function(){
+      var campsite = {};
 
-      var city     = $(this).find('a').text(),
-          facebook = $(this).find('a').attr("href"),
-          region   = $(this).find('ul > li > ul').text().trim(),
-          country  = $( $(this).contents().get(0) ).text().trim();
+      campsite.city = $(this).text().trim();
+      // Each city's containint unordered list
+      var cityList = $( $(this).parent() ).parent();
+      // Element containing the name of the city's region name (if city is not within a region, element contains country name)
+      var element = $(cityList).parent().contents().get(0);
 
-      data = {"country":country,"region":region,"city":city};
+      // Check if element is at the top of the UL trees.
+      if(!$( $( $(element).parent() ).parent() ).parent().hasClass('markdown-body')){
+        campsite.region = $( element ).text().trim();
+        campsite.country = $( $( $( $(element).parent() ).parent() ).parent().contents().get(0) ).text().trim();
+      } else {
+        campsite.country = $( element ).text().trim();
+      }
 
-      json.push(data);
+      campsite.facebook = $(this).attr('href').trim();
+
+      json.push(campsite);
     });
 
-  res.send(json);
+  res.json(json);
   });
 });
+
 
 app.listen(8080);
 console.log('Listening on port 8080');
 exports = module.exports = app;
-
-
-
-
-
-// country
-//   region
-//   region
-//     city
-//     city
